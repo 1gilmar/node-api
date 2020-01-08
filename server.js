72f7120edefec78b1e8debf0167cf73d4065651b
jiraspiom <jiraspiom@yahoo.com.br>
2020-01-08 09:27:38 -0300

    criado o arquivo principal app e subindo o servido na porta 30001

---
+++ server.js
@@ -0,0 +1,8 @@
+//importando o express
+const express = require('express')
+
+//definindo uma contate e executar funcao express
+const app = express();
+
+//fazendo a aplicacao ouvir a porta 3001
+app.listen(3001);
