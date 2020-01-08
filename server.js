42cff54bffaa7423d0864c7ba1b073f93561ca36
jiraspiom <jiraspiom@yahoo.com.br>
2020-01-08 13:29:44 -0300

    inicinando o banco de dados

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 //importando o express
 const express = require('express')
+const mongoose = require('mongoose')
 
-//definindo uma contate e executar funcao express
+//definindo uma contate e executar funcao express; iniciando o app
 const app = express();
+
+//inicionado o banco de dados
+mongoose.connect('mongodb://localhost:27017/mongoapi');
 
 //criando uma rota
 app.get("/",(req, res) => {
-    res.send("Hello mundo")
+    res.send("Hello mundo");
 });
 
 app.get("/pessoa",(req, res) => {
-    res.send("alterado para verifiar o nodemon sincronizar")
+    res.send("alterado para verifiar o nodemon sincronizar");
 });
 
 //fazendo a aplicacao ouvir a porta 3001
