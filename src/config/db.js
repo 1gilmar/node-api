82a246c2b12bec330cc8fa4989e70cc40816ef82
jiraspiom <jiraspiom@yahoo.com.br>
2022-10-07 15:14:22 -0300

    ajusdado

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV == "production"){
+if(process.env.NODE_ENV != "production"){
     module.exports = {mongoURI: "mongodb+srv://root:jiraspiom@cluster0-2gq4v.gcp.mongodb.net/test?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost:27017/nodeapi"}
